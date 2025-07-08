77ae4450f5b94b0cfbbe299e27c91c7e352fdbce
k2effecteducate <126368223+k2effecteducate@users.noreply.github.com>
2025-07-08 12:24:57 -0500

    added pinpoint, these are for reference

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/SensorPinpointDriveToPoint.java
@@ -0,0 +1,154 @@
+package org.firstinspires.ftc.teamcode.decode;
+
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+
+import java.util.Locale;
+
+@Autonomous(name = "Pinpoint Navigation Example", group = "Pinpoint")
+//@Disabled
+
+public class SensorPinpointDriveToPoint extends LinearOpMode {
+
+    DcMotor leftFrontDrive;
+    DcMotor rightFrontDrive;
+    DcMotor leftBackDrive;
+    DcMotor rightBackDrive;
+
+    GoBildaPinpointDriver pinpoint; // Declare OpMode member for the Odometry Computer
+    DriveToPoint nav = new DriveToPoint(this); //OpMode member for the point-to-point navigation class
+
+    enum StateMachine {
+        WAITING_FOR_START,
+        AT_TARGET,
+        DRIVE_TO_TARGET_1,
+        DRIVE_TO_TARGET_2,
+        DRIVE_TO_TARGET_3,
+        DRIVE_TO_TARGET_4,
+        DRIVE_TO_TARGET_5
+    }
+
+    static final Pose2D TARGET_1 = new Pose2D(DistanceUnit.MM, 2000, 20, AngleUnit.DEGREES, 0);
+    static final Pose2D TARGET_2 = new Pose2D(DistanceUnit.MM, 2600, -20, AngleUnit.DEGREES, -90);
+    static final Pose2D TARGET_3 = new Pose2D(DistanceUnit.MM, 2600, -2600, AngleUnit.DEGREES, -90);
+    static final Pose2D TARGET_4 = new Pose2D(DistanceUnit.MM, 100, -2600, AngleUnit.DEGREES, 90);
+    static final Pose2D TARGET_5 = new Pose2D(DistanceUnit.MM, 100, 0, AngleUnit.DEGREES, 0);
+
+
+    @Override
+    public void runOpMode() {
+
+        // Initialize the hardware variables. Note that the strings used here must correspond
+        // to the names assigned during the robot configuration step on the DS or RC devices.
+
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "frontLeft");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "frontRight");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "backLeft");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "backRight");
+
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftFrontDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        pinpoint = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
+        pinpoint.setOffsets(-84.0, -168.0, DistanceUnit.MM);
+        pinpoint.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+        pinpoint.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.REVERSED, GoBildaPinpointDriver.EncoderDirection.FORWARD);
+
+        pinpoint.resetPosAndIMU();
+
+        //nav.setXYCoefficients(0.02,0.002,0.0,DistanceUnit.MM,12);
+        //nav.setYawCoefficients(1,0,0.0, AngleUnit.DEGREES,2);
+        nav.setDriveType(DriveToPoint.DriveType.MECANUM);
+
+        StateMachine stateMachine;
+        stateMachine = StateMachine.WAITING_FOR_START;
+        Pose2D pose2D = pinpoint.getPosition();
+        telemetry.addData("Status", "Initialized");
+        telemetry.addData("X coordinate (IN)", pose2D.getX(DistanceUnit.INCH));
+        telemetry.addData("Y coordinate (IN)", pose2D.getY(DistanceUnit.INCH));
+        telemetry.addData("Device Version Number:", pinpoint.getDeviceVersion());
+        telemetry.addData("Device Scalar", pinpoint.getYawScalar());
+        telemetry.update();
+
+        // Wait for the game to start (driver presses START)
+        waitForStart();
+        resetRuntime();
+
+        while (opModeIsActive()) {
+            pinpoint.update();
+
+            switch (stateMachine) {
+                case WAITING_FOR_START:
+                    //the first step in the autonomous
+                    stateMachine = StateMachine.DRIVE_TO_TARGET_1;
+                    break;
+                case DRIVE_TO_TARGET_1:
+                    /*
+                    drive the robot to the first target, the nav.driveTo function will return true once
+                    the robot has reached the target, and has been there for (holdTime) seconds.
+                    Once driveTo returns true, it prints a telemetry line and moves the state machine forward.
+                     */
+                    if (nav.driveTo(pinpoint.getPosition(), TARGET_1, 0.7, 0)) {
+                        telemetry.addLine("at position #1!");
+                        stateMachine = StateMachine.DRIVE_TO_TARGET_2;
+                    }
+                    break;
+                case DRIVE_TO_TARGET_2:
+                    //drive to the second target
+                    if (nav.driveTo(pinpoint.getPosition(), TARGET_2, 0.7, 1)) {
+                        telemetry.addLine("at position #2!");
+                        stateMachine = StateMachine.DRIVE_TO_TARGET_3;
+                    }
+                    break;
+                case DRIVE_TO_TARGET_3:
+                    if (nav.driveTo(pinpoint.getPosition(), TARGET_3, 0.7, 3)) {
+                        telemetry.addLine("at position #3");
+                        stateMachine = StateMachine.DRIVE_TO_TARGET_4;
+                    }
+                    break;
+                case DRIVE_TO_TARGET_4:
+                    if (nav.driveTo(pinpoint.getPosition(), TARGET_4, 0.7, 1)) {
+                        telemetry.addLine("at position #4");
+                        stateMachine = StateMachine.DRIVE_TO_TARGET_5;
+                    }
+                    break;
+                case DRIVE_TO_TARGET_5:
+                    if (nav.driveTo(pinpoint.getPosition(), TARGET_5, 0.7, 1)) {
+                        telemetry.addLine("There!");
+                        stateMachine = StateMachine.AT_TARGET;
+                    }
+                    break;
+            }
+
+
+            //nav calculates the power to set to each motor in a mecanum or tank drive. Use nav.getMotorPower to find that value.
+            leftFrontDrive.setPower(nav.getMotorPower(DriveToPoint.DriveMotor.LEFT_FRONT));
+            rightFrontDrive.setPower(nav.getMotorPower(DriveToPoint.DriveMotor.RIGHT_FRONT));
+            leftBackDrive.setPower(nav.getMotorPower(DriveToPoint.DriveMotor.LEFT_BACK));
+            rightBackDrive.setPower(nav.getMotorPower(DriveToPoint.DriveMotor.RIGHT_BACK));
+
+            telemetry.addData("current state:", stateMachine);
+
+            Pose2D pos = pinpoint.getPosition();
+            String data = String.format(Locale.US, "{X: %.3f, Y: %.3f, H: %.3f}", pos.getX(DistanceUnit.MM), pos.getY(DistanceUnit.MM), pos.getHeading(AngleUnit.DEGREES));
+            telemetry.addData("Position", data);
+
+            telemetry.update();
+
+        }
+    }
+}
